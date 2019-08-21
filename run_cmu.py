fd81bce189ced9fccc9a4276618ec2e8b3f73744
M Usama Alvi <m.usamaalvi@gmail.com>
2019-08-21 09:36:28 +0500

    Working fine now.

--- run_cmu.py
+++ run_cmu.py
@@ -4,12 +4,14 @@ from flask import Flask, render_template, request
 import os, sys
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from socketserver import ThreadingMixIn
-
+from flask_restful import Resource, Api
 from threading import Thread, Timer, Lock, Event
-
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+api = Api(app)
+app.debug = True
+CORS(app)
 
 @app.route('/')
 def index():
@@ -27,6 +29,18 @@ def run_code():
         print(e)
     return('', 204)
     
-
+@app.route('/IFrameCodeRun', methods=['POST'])
+def SolutionCode():
+	print("came in")
+	if request.method == "POST":
+		content = request.get_json()
+		code = content['code']
+		code = "from templates.cmu_graphics import *\nresetGlobals()\n" + code + "\nrun()\n"
+		print(code)
+		try:
+			exec(code)
+		except Exception as e:
+			print(e)
+		jsonify({'result':'testing'})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1000)
